9ddf7eb371d278bd9bfdfff563a3134b9e20635e
Kyle Karacadag <kylekaracadag1@gmail.com>
2021-08-13 19:43:06 -0400

    Added score table information

--- webscraper.py
+++ webscraper.py
@@ -63,17 +63,17 @@ def get_league_position(soup):
 
 
 def get_score_information(soup):
-    table_information = []
+    table_information = {}
     all = soup.find_all("table")
     table = all[4].find_all("tr")
     table = table[1:]
 
     for index, _ in enumerate(table):
-        score_dictionary = {}
         score_list = []
         team_name = table[index].find_all("td", {"class": "no-border-links hauptlink"})
         team_name = team_name[0].find_all("a", {"class": "vereinprofil_tooltip"})
         name = team_name[0].text.strip()
+        name = name.split('.')[-1].strip()
         
         score_info = table[index].find_all("td", {"class": "zentriert"})[2:]
         for index, _ in enumerate(score_info):
@@ -82,8 +82,7 @@ def get_score_information(soup):
             else:
                 score_list.append(score_info[index].text.strip())
             
-        score_dictionary[name] = score_list
-        table_information.append(score_dictionary)
+        table_information[name] = score_list
 
     return table_information
 
@@ -114,6 +113,22 @@ def get_match_results(soup):
 
     return result_list
 
+def clean_score_list(score_list):
+    table_information = []
+
+    table_information.append(int(score_list[0])) # NumWins
+    table_information.append(int(score_list[1])) # NumDraws
+    table_information.append(int(score_list[2])) # NumLosses
+
+    goals = score_list[3].split(':')
+    table_information.append(int(goals[0])) # GoalsScored
+    table_information.append(int(goals[1])) #GoalsConceded
+
+    table_information.append(int(score_list[4])) # GoalAvg
+    table_information.append(int(score_list[5])) # Points
+
+    return table_information
+
 
 def main():
     fixture_data = []
@@ -121,17 +136,24 @@ def main():
 
     for season in range(2005, 2006):
         print(f"\nAdding Data for Season {season}/{season+1}\n")
-        for matchday in range(1, 3):
+        for matchday in range(2, 3):
             season_requests = requests.get(fixture_url + str(season) + '&spieltag=' + str(matchday), 
+                headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
+            prev_season_requests = requests.get(fixture_url + str(season) + '&spieltag=' + str(matchday-1), 
                 headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
             
             content = season_requests.content
             soup = BeautifulSoup(content, 'html.parser')
+            prev_content = prev_season_requests.content
+            prev_soup = BeautifulSoup(prev_content, 'html.parser')
 
             home_team_list, away_team_list = get_fixture_list(soup)
             fixture_list = get_fixture_text(home_team_list, away_team_list)
             home_positions, away_positions = get_league_position(soup)
             result_list = get_match_results(soup)
+            score_info = {}
+            if matchday != 1:
+                score_info = get_score_information(prev_soup)
 
             for index, _ in enumerate(fixture_list):
                 tmp_dictionary = {}
@@ -144,9 +166,49 @@ def main():
                 if matchday == 1:
                     tmp_dictionary['HomePositions'] = 0
                     tmp_dictionary['AwayPositions'] = 0
+                    tmp_dictionary['HomeWins'] = 0
+                    tmp_dictionary['AwayWins'] = 0
+                    tmp_dictionary['HomeDraws'] = 0
+                    tmp_dictionary['AwayDraws'] = 0
+                    tmp_dictionary['HomeLosses'] = 0
+                    tmp_dictionary['AwayLosses'] = 0
+                    tmp_dictionary['HomeGoalsScored'] = 0
+                    tmp_dictionary['AwayGoalsScored'] = 0
+                    tmp_dictionary['HomeGoalsConceded'] = 0
+                    tmp_dictionary['AwayGoalsConceded'] = 0
+                    tmp_dictionary['HomeGoalDiff'] = 0
+                    tmp_dictionary['AwayGoalDiff'] = 0
+                    tmp_dictionary['HomePoints'] = 0
+                    tmp_dictionary['AwayPoints'] = 0
                 else:
+                    home_team_name = home_team_list[index]
+                    away_team_name = away_team_list[index]
+                    home_score_list = []
+                    away_score_list = []
+                    for key in score_info:
+                        if key in home_team_name:
+                            home_score_list = clean_score_list(score_info[key])
+                        if key in away_team_name:
+                            away_score_list = clean_score_list(score_info[key])
+
                     tmp_dictionary['HomePositions'] = home_positions[index]
                     tmp_dictionary['AwayPositions'] = away_positions[index]
+                    tmp_dictionary['HomeWins'] = home_score_list[0]
+                    tmp_dictionary['AwayWins'] = away_score_list[0]
+                    tmp_dictionary['HomeDraws'] = home_score_list[1]
+                    tmp_dictionary['AwayDraws'] = away_score_list[1]
+                    tmp_dictionary['HomeLosses'] = home_score_list[2]
+                    tmp_dictionary['AwayLosses'] = away_score_list[2]
+                    tmp_dictionary['HomeGoalsScored'] = home_score_list[3]
+                    tmp_dictionary['AwayGoalsScored'] = away_score_list[3]
+                    tmp_dictionary['HomeGoalsConceded'] = home_score_list[4]
+                    tmp_dictionary['AwayGoalsConceded'] = away_score_list[4]
+                    tmp_dictionary['HomeGoalDiff'] = home_score_list[5]
+                    tmp_dictionary['AwayGoalDiff'] = away_score_list[5]
+                    tmp_dictionary['HomePoints'] = home_score_list[6]
+                    tmp_dictionary['AwayPoints'] = away_score_list[6]
+
+
 
                 # Add the match scores to the dictionary
                 tmp_dictionary['Result'] = result_list[index]
