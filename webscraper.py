74a0c7605e0e8045681534071c19e54c60d8243e
Kyle Karacadag <kylekaracadag1@gmail.com>
2021-08-13 20:56:53 -0400

    Added comments

--- webscraper.py
+++ webscraper.py
@@ -4,22 +4,41 @@ import pandas
 
 
 def get_fixture_list(soup):
+    """Get the list of every fixture in given week
+
+    This function returns the name of the home and away team
+    for the games being played in a given week.
+
+    Args:
+        soup: The parsed HTML data that we got from BeautifulSoup 
+        on the link for the given week's fixture page on transfermarkt
+
+    Returns:
+        The first list inclduing the name of all of the home teams
+        for the current week and the name of all the away teams for
+        the same week
+    """
+
     fixture_list = []
     all = soup.find_all("div", {"class": "responsive-table"})
 
     # Get the list of home teams for given week
-    home_games = all[0].find_all("td", {"class": "text-right no-border-rechts no-border-links hauptlink hide-for-small"})
+    home_games = all[0].find_all("td", 
+        {"class": "text-right no-border-rechts no-border-links hauptlink hide-for-small"})
     home_team_list = []
     for index, _ in enumerate(home_games):
-        home_game = home_games[index].find_all("a", attrs={'class': "vereinprofil_tooltip"})
+        home_game = home_games[index].find_all("a", 
+            attrs={'class': "vereinprofil_tooltip"})
         for tag in home_game:
             home_team_list.append(tag.text.strip())
 
     # Get the list of away teams for given week
-    away_games = all[0].find_all("td", {"class": "no-border-links no-border-rechts hauptlink hide-for-small"})
+    away_games = all[0].find_all("td", 
+        {"class": "no-border-links no-border-rechts hauptlink hide-for-small"})
     away_team_list = []
     for index, _ in enumerate(away_games):
-        away_game = away_games[index].find_all("a", attrs={"vereinprofil_tooltip"})
+        away_game = away_games[index].find_all("a", 
+            attrs={"vereinprofil_tooltip"})
         for tag in away_game:
             away_team_list.append(tag.text.strip())
 
@@ -29,7 +48,19 @@ def get_fixture_list(soup):
 
     return home_team_list, away_team_list
 
+
 def get_fixture_text(home_team_list, away_team_list):
+    """Convert the fixture list into readable text
+
+    Args:
+        home_team_list: The list of home teams playing for the current week
+
+        away_team_list: The list of away teams playing for the current week
+
+    Returns:
+        Combined name of the home team and away team for each game in the week
+    """
+
     fixture_list = []
 
     for home, away in zip(home_team_list, away_team_list):
@@ -39,6 +70,18 @@ def get_fixture_text(home_team_list, away_team_list):
 
 
 def get_league_position(soup):
+    """Get the league position of each team in the current week
+
+    Args:
+        soup: The parsed HTML data that we got from BeautifulSoup 
+        on the link for the given week's fixture page on transfermarkt
+
+    Returns:
+        The first list inclduing the league position of all of the home teams
+        for the current week and the league position of all the away teams for
+        the same week
+    """
+
     home_positions = []
     away_positions = []
     position_list = []
@@ -53,7 +96,8 @@ def get_league_position(soup):
         
     # Clean the position list
     league_position_list = []
-    [league_position_list.append(x) for x in position_list if x not in league_position_list]
+    [league_position_list.append(x) \
+        for x in position_list if x not in league_position_list]
 
     # Divide the home team positions and the away team positions
     home_positions = league_position_list[::2]
@@ -63,15 +107,36 @@ def get_league_position(soup):
 
 
 def get_score_information(soup):
+    """Get the score information of each team in the current week
+
+    This function returns the name of each team playing in the current
+    week and their current number of wins, draws, losses, goals scored,
+    goals conceded, goal average, and their current points. 
+
+    Args:
+        soup: The parsed HTML data that we got from BeautifulSoup 
+        on the link for the given week's fixture page on transfermarkt
+
+    Returns:
+        A dictionary that has the team name as the key and a list of all
+        the information listed above as the value
+    """
+
     table_information = {}
     all = soup.find_all("table")
     table = all[4].find_all("tr")
     table = table[1:]
 
+    # Iterate over all the teams in the league standings table
     for index, _ in enumerate(table):
         score_list = []
-        team_name = table[index].find_all("td", {"class": "no-border-links hauptlink"})
-        team_name = team_name[0].find_all("a", {"class": "vereinprofil_tooltip"})
+        team_name = table[index].find_all("td", 
+            {"class": "no-border-links hauptlink"})
+        team_name = team_name[0].find_all("a", 
+            {"class": "vereinprofil_tooltip"})
+
+        # Since the websites stores different names on the board and fixture
+        # list we trim down the name of the team to only include the main name
         name = team_name[0].text.strip()
         name = name.split('.')[-1].strip()
         
@@ -88,6 +153,22 @@ def get_score_information(soup):
 
 
 def get_match_results(soup):
+    """Get the match results of each fixture in the current week
+
+    This functions returns the result of each fixture: 
+        0 if the game ends in a draw
+        1 if the home team wins the game and away team loses
+        2 if the away team wins the game and home team loses
+
+    Args:
+        soup: The parsed HTML data that we got from BeautifulSoup 
+        on the link for the given week's fixture page on transfermarkt
+
+    Returns:
+        A list with the result of each game based on the numbering
+        mentioned above
+    """
+    
     result_list = []
     all = soup.find_all("div", {"class": "responsive-table"})
     match_results = all[0].find_all("span", {"class": "matchresult finished"})
@@ -113,7 +194,20 @@ def get_match_results(soup):
 
     return result_list
 
+
 def clean_score_list(score_list):
+    """Clean up the list with score information
+
+    This function converts all the values from score list that we
+    got from the get_score_information function into an integer
+
+    Args:
+        score_list: score_list that the get_score_information function returned
+
+    Returns:
+        The same list as the score_list but all the values are converted to int
+    """
+
     table_information = []
 
     table_information.append(int(score_list[0])) # NumWins
@@ -137,9 +231,11 @@ def main():
     for season in range(2005, 2006):
         print(f"\nAdding Data for Season {season}/{season+1}\n")
         for matchday in range(1, 3):
-            season_requests = requests.get(fixture_url + str(season) + '&spieltag=' + str(matchday), 
+            season_requests = requests.get(
+                fixture_url + str(season) + '&spieltag=' + str(matchday), 
                 headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
-            prev_season_requests = requests.get(fixture_url + str(season) + '&spieltag=' + str(matchday-1), 
+            prev_season_requests = requests.get(
+                fixture_url + str(season) + '&spieltag=' + str(matchday-1), 
                 headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
             
             content = season_requests.content
@@ -162,7 +258,8 @@ def main():
                 tmp_dictionary['Matchday'] = matchday
                 
                 # Add the matches to the dictionary
-                tmp_dictionary['Matches'] = f"{str(season)[2:]}/{str(season+1)[2:]} - {fixture_list[index]}"
+                m = f"{str(season)[2:]}/{str(season+1)[2:]} - {fixture_list[index]}"
+                tmp_dictionary['Matches'] = m
 
                 # Add the home and away positions to the dictionary
                 # For the first week set league position values to 0
@@ -211,8 +308,6 @@ def main():
                     tmp_dictionary['HomePoints'] = home_score_list[6]
                     tmp_dictionary['AwayPoints'] = away_score_list[6]
 
-
-
                 # Add the match scores to the dictionary
                 tmp_dictionary['Result'] = result_list[index]
 
@@ -220,7 +315,7 @@ def main():
 
             print(f"Successfully Added Season {season}/{season+1} Matchday {matchday}")
 
-
+    # Convert the data into a csv file
     df = pandas.DataFrame(fixture_data)
     df.to_csv("TeamData.csv")
         
@@ -229,8 +324,9 @@ if __name__ == "__main__":
     main()
 
 
-# [Done] Match
-# Matchday
+# [Done] Matchday
+# [Done] Matches
+# [Done] HomePositions / AwayPositions
 # [Done] HomeWins / AwayWins
 # [Done] HomeDraws / AwayDraws
 # [Done] HomeLosses / AwayLosses
@@ -238,15 +334,14 @@ if __name__ == "__main__":
 # [Done] HomeGoalsConceded / AwayGoalsConceded
 # [Done] HomeGoalDiff / AwayGoalDiff
 # [Done] HomePoints / AwayPoints
+# [Done] Result
 
 # Link for home team / Link for away team
 # HomeMarketVal / AwayMarketVal
-# [Done] HomePositions / AwayPositions
 # HomeAvgAge / AwayAvgAge
 # HomeNumForeigners / AwayNumForeigners
 # HomeStadiumCap
 # HomeNumNationalPlayers / AwayNumNationalPlayers
-# [Done] Result
 
 # OPTIONAL
 # HomeInjuredMarketVal / AwayInjuredMarketVal
